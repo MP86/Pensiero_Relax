d623981504274dedbf625ba11829f4fde2ccea1e
ftygvhgjh <hyhgyh>
2016-03-24 12:07:05 +0100

    Aggiumto cartella home

---
+++ resources/views/home/rilassante.blade.php
@@ -0,0 +1,129 @@
+@extends('master')
+
+
+@section('seo')
+	<meta name="description" content="Massaggi Shiatsu e rilassanti anche a casa tua.
+	I motivi per scegliere il massaggio rilassante.">
+    <meta name="author" content="Pensiero Relax - MP86">
+@stop
+
+@section('title')
+Rilassante
+@stop
+
+@section('content')
+
+<div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Il massaggio
+                        <strong>rilassante:</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-md-6">
+                    <img class="img-responsive img-border-left" src="../img/slide1.jpg" alt="massaggio rilassante">
+                </div>
+                <div class="col-md-6">
+                	<h3>Storia del massaggio rilassante:</h3>
+                    <p>L’origine di questa pratica risale a molti secoli fa e probabilmente il massaggio è la forma
+                     più antica di trattamento medico utilizzata per eliminare la fatica, alleviare il dolore,
+                      rilassare e consentire una più facile applicazione di oli e unguenti sulla pelle.
+                       I primi riferimenti storici sono nei manoscritti cinesi del 2700 a.C. e in testi antichi
+                        di medicina indiana. Nei secoli dall’Oriente questa pratica si diffonde nel resto del mondo
+                        assumendo diverse funzioni. Così oltre al massaggio rilassante ci sono quello drenante,
+                         sportivo, anticellulite, la riflessologia plantare e molti altri,
+                         per rispondere alle esigenze di tutti.
+                        Il massaggio rilassante resta però la forma più comune di questa pratica 
+                        e ha come scopo quello di favorire il rilassamento e donare consapevolezza del proprio corpo.
+                        Per questo motivo la tecnica può variare molto in base alla persona e svolgersi
+                         con diverse modalità.</p>
+                    </div>
+                <div class="col-md-6">
+                    <img class="img-responsive img-border-left" src="../img/slide1.jpg" alt="massaggio rilassante">
+                </div>
+                
+                <div class="col-md-6">
+                    <h3>Le tipologie di massaggio rilassante:</h3>
+                    <p>Solitamente il massaggio rilassante viene fatto sulle spalle e sulla parte bassa della schiena,
+                     dove si accumulano maggiormente le tensioni muscolari, ma può comprendere anche piedi,
+                      braccia e mani per raggiungere un relax totale.<br><br><br><br></p>
+                      <br>
+                      <br>
+                 </div>
+                 
+                <div class="col-md-6">
+                    <img class="img-responsive img-border-left" src="../img/slide1.jpg" alt="massaggio rilassante">
+                </div>
+                     
+                <div class="col-md-6">
+                    <h3>Come si svolge il massaggio rilassante:</h3>
+                    <p>Il massaggio viene eseguito in un ambiente che favorisce il relax fisico e mentale,
+                     con luci soffuse e/o candele: ci si stende su un lettino e vengono applicati olii o essenze
+                      per favorire il frizionamento della pelle e per rilasciare una piacevole profumo.
+                       Le mani del massaggiatore alternano movimenti fluidi a leggere pressioni esercitate con i palmi
+                        per sciogliere le tensioni muscolari e regalare un benessere psico-fisico. 
+                        Si ottiene così un profondo rilassamento, come dopo alcune ore di sonno, 
+                        aumenta la flessibilità del corpo e migliora la circolazione sanguigna,
+                         donando una sensazione di intensa vitalità.</p>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        
+        <div class="col-lg-12 text-center">
+                    <h2>Rilassante
+                        <br>
+                        <small>2016</small>
+                    </h2>
+                    
+                    <p>Il massaggio rilassante <br>
+                    pensato per abbattere totalmente lo stress accumulato.
+                    <br><br>
+                    <small>DURATA: </small>
+                    <br>Parziale: 30 Min. (circa)
+                    <br>Totale: 50 Min. (circa)
+                    </p>
+					<br>
+                    <a href="/rilassante" class="btn btn-default btn-lg">Per Saperne Di Più</a>
+                    <hr>
+           </div>
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Our
+                        <strong>Team</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                    <h3>John Smith
+                        <small>Job Title</small>
+                    </h3>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                    <h3>John Smith
+                        <small>Job Title</small>
+                    </h3>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                    <h3>John Smith
+                        <small>Job Title</small>
+                    </h3>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+    
+@stop
